661fc776c316f00ad1fa1bfe26eb9f9ba388d543
andreitipo <56038309+andreitipo@users.noreply.github.com>
2020-05-15 18:52:28 +0300

    Add files via upload

---
+++ CSharp/3LR/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+namespace _3labaratornaya
+{
+   
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            int number = 2;
+            Random rnd = new Random();
+
+            Vehicle.Id = rnd.Next(1, 99999);
+            Motorcade numOfVehicle = new Motorcade(number);
+            numOfVehicle[0] = new Vehicle("bmw",2000,"Germany");
+            numOfVehicle[0].setValues("Green",12000 ,240);
+            numOfVehicle[0].GetInfo();
+            numOfVehicle[0].GetMaxSpeed();
+            numOfVehicle[0].GetColor();
+            Console.WriteLine("\n");
+            Vehicle.Id = rnd.Next(1, 99999);
+            Vehicle myCar = new Vehicle("Ford",1988,"UK");
+            myCar.Price= 35000;
+            myCar.Color = "Blue";
+            myCar.MaxSpeed = 220;
+            myCar.GetInfo();
+            myCar.GetColor();
+            myCar.GetMaxSpeed();
+            
+           
+        }
+    }
+}
---
+++ CSharp/3LR/Vehicle.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace _3labaratornaya
+{
+    class Vehicle
+    {
+        
+        // fields
+        private float price;
+        public static int Id;
+        private int seats;
+        //private int year;
+        // auto-properties
+        public string Name { get; set; }
+        public float MaxSpeed { get; set; }
+        public string Color { get; set; }
+        public string CountryOfManufacture { get; set; }
+        public int Year {get;set;}   
+        public float Price
+        {
+            set
+            {
+                if (value > 0)
+                {
+                    try
+                    {
+                        price = value;
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("Incorrect input : {0}", e.Message);
+                    }
+                }
+            }
+            get
+            {
+                return price;
+            }
+        }
+        public int Seats
+        {
+            get
+            {
+                return seats;
+            }
+            set
+            {
+                if (seats > 0)
+                {
+
+
+                    try
+                    {
+                        seats = value;
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("Incorrect input : {0}", e.Message);
+                    }
+                    }
+            }
+        }
+       
+
+        public Vehicle()
+        {
+
+        }
+        public Vehicle(string name,int year,string country) 
+        {
+            Name = name;
+            Year = year;
+            CountryOfManufacture = country;    
+        }
+
+
+        // function 
+
+        public void setValues(string name,string myCountryOfManufacture)
+        {
+            Name = name;        
+            CountryOfManufacture = myCountryOfManufacture;
+        }
+        public void setValues(string mycolor,float price,float mymaxspeed)
+        {
+            this.price = price;
+            Color = mycolor;
+            MaxSpeed = mymaxspeed;
+        }
+        public void GetInfo()
+        {
+           
+            Console.WriteLine("Vehicle name: {0}\nYear of manufacture : {1} \nCountryOfManufacture : {2} \nPrice : {3} $\nId : {4}",Name,Year,CountryOfManufacture,Price,Id);
+        }
+        public void GetMaxSpeed()
+        {
+            Console.WriteLine("MaxSpeed : {0} km/h", MaxSpeed);
+        }  
+        public void GetColor()
+        {
+            Console.WriteLine("Color : {0}", Color);
+        }
+
+    }
+    class Motorcade
+    {
+        Vehicle[] data;
+        public Motorcade(int number)
+        {
+            data = new Vehicle[number];
+
+
+        }
+        public Vehicle this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+            set
+            {
+                data[index] = value;
+            }
+        }
+    }
+}
