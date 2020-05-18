08f6fde44ed3664fcb2f727f531d7e65f268d993
andreitipo <56038309+andreitipo@users.noreply.github.com>
2020-05-18 03:09:14 +0300

    Add files via upload

---
+++ CSharp/4Lab/1part/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+namespace lab4
+{
+    static class Program
+    {
+
+
+        [DllImport("User32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        static void Main()
+        {
+            Int32  vk_Escape = 27; 
+            Console.WriteLine("Click \"Esc\" to finish");
+            while (true)
+            {
+                Thread.Sleep(5);
+                if (GetAsyncKeyState(vk_Escape) < 0)
+                    {
+                        break;
+                    }
+ 
+                
+                for (Int32 i = 0; i < 255; i++)
+                {
+                    int temp = GetAsyncKeyState(i);
+                    if (temp == 32769)
+                    {
+
+                        Console.WriteLine((Keys)i);
+
+                    }
+                }
+            }
+
+        }
+
+    }
+}
